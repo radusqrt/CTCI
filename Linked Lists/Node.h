acbbd921bdc8c765a4a717f03882aab08b9a6be7
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2015-10-27 17:30:32 +0200

    Leaving to EEA

--- Linked Lists/Node.h
+++ Linked Lists/Node.h
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <iostream>
 
 class Node
 {
@@ -6,9 +7,48 @@ public:
 	int data;
 	Node *next;
 
-	Node(int data)
+	Node()
 	{
-		this->data = data;
 		next = NULL;
 	}
+
+	Node(int data)
+	{	
+		Node();
+		this->data = data;
+	}
+
+	void read(int n)
+	{
+		int data;
+		if(n == 0) return;
+		std::cin >> data;
+		this->data = data;
+		n--;
+		if(n > 0)
+		{
+			Node *aux = new Node();
+			next = aux;
+			aux->read(n);
+		}
+	}
+
+	void print()
+	{
+		std::cout << data << " ";
+		if(next != NULL)
+			next->print();
+	}
 };
+
+void readList(Node *first, int n)
+{
+	int data;
+	if(n == 0) return;
+	std::cin >> data;
+	first = new Node(data);
+
+	Node *next = NULL;
+	first->next = next;
+	readList(next, n-1);
+}
