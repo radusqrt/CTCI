6caa9b328b201282a5708dae73c337a0287f022b
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2015-11-17 02:45:48 +0200

    Sorting and Searching DONE

--- Concepts and Algorithms/Sorting and Searching/6.cpp
+++ Concepts and Algorithms/Sorting and Searching/6.cpp
@@ -11,31 +11,36 @@ void binarySearch(int **a, int leftUpX, int leftUpY,
 
   if(rightDownX == leftUpX && rightDownY == leftUpY)
   {
-    if(a[mX][mY] == key)
+    if(a[rightDownX][rightDownY] == key)
     {
-      posX
+      posX = rightDownX;
+      posY = rightDownY;
     }
+    return;
+  }
 
   int mX = (leftUpX + rightDownX) / 2;
   int mY = (leftUpY + rightDownY) / 2;
+
   if(a[mX][mY] == key)
   {
     posX = mX, posY = mY;
     return;
   }
-  if(mX+1 < NMAX && mY+1 < MMAX)
-  if(a[mX+1][mY+1] == key)
-  {
-    posX = mX+1, posY = mY+1;
-    return;
-  }
 
   if(mX+1 < NMAX && mY+1 < MMAX)
-  if(a[mX+1][mY+1] < key)
-  {
-    binarySearch(a, mX+1, mY+1, rightDownX, rightDownY, key, posX, posY);
-    return;
-  }
+    if(a[mX+1][mY+1] == key)
+    {
+      posX = mX+1, posY = mY+1;
+      return;
+    }
+
+  if(mX+1 < NMAX && mY+1 < MMAX)
+    if(a[mX+1][mY+1] < key)
+    {
+     binarySearch(a, mX+1, mY+1, rightDownX, rightDownY, key, posX, posY);
+     return;
+    }
 
   if(mX+1 < NMAX)
     binarySearch(a, mX+1, leftUpY, rightDownX, mY, key, posX, posY);
@@ -63,16 +68,16 @@ int main()
 
   int posX, posY, key;
 
-  // for(i = 0; i < n; ++i)
-    // for(j = 0; j < m; ++j)
-    // {
-      // key = a[i][j];
-      // binarySearch(a, 0, 0, n-1, m-1, key, posX, posY);
-      // cout << posX << " " << posY << "\n";
-    // }
+  for(i = 0; i < n; ++i)
+    for(j = 0; j < m; ++j)
+    {
+      key = a[i][j];
+      binarySearch(a, 0, 0, n-1, m-1, key, posX, posY);
+      cout << posX << " " << posY << "\n";
+    }
 
-    key = a[0][0];
-    binarySearch(a, 0, 0, n-1, m-1, key, posX, posY);
-    cout << posX << " " << posY << "\n";
+    // key = a[0][0];
+    // binarySearch(a, 0, 0, n-1, m-1, key, posX, posY);
+    // cout << posX << " " << posY << "\n";
   return 0;
 }
